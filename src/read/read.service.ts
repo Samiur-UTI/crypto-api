4cd9a5b41bc5b59ea36d744eaeea8662251808fc
Samiur-UTI <samiurkhan94@gmail.com>
2022-04-17 02:14:59 +0600

    csv converted to json

--- src/read/read.service.ts
+++ src/read/read.service.ts
@@ -2,37 +2,46 @@ import {
   Injectable,
   BadRequestException,
   NotFoundException,
+  StreamableFile,
 } from '@nestjs/common';
+import { CsvParser } from 'nest-csv-parser';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as es from 'event-stream';
 import { Transactions } from 'src/entities/transaction.entity';
-const dirPath = path.join(__dirname, '../../src/assets/transactions.csv');
+const dirPathRead = path.join(__dirname, '../../src/assets/transactions.csv');
+const dirPathWrite = path.join(__dirname, '../../src/assets/transactions.json');
+
 @Injectable()
 export class ReadService {
-  constructor() {}
+  constructor(private readonly csvParser: CsvParser) {}
   async read(): Promise<any> {
-    const readStream = fs.createReadStream(dirPath);
-    function streamReader(stream: fs.ReadStream) {
-      return new Promise<any>((resolve, reject) => {
-        const transactions: Transactions[] = [];
-        stream.on('data', (chunk) => {
-          const line = chunk.toString();
-          const [timestamp, transactionType, token, amount] = line.split(',');
-          transactions.push({
-            timestamp: Number(timestamp),
-            transactionType: transactionType,
-            token: token,
-            amount: Number(amount),
-          });
-        });
-        stream.on('end', () => {
-          resolve(transactions);
-        });
-        stream.on('error', (err) => {
-          reject(err);
-        });
+    const readStream = fs.createReadStream(dirPathRead);
+    const writeStream = fs.createWriteStream(dirPathWrite);
+    let count = 0;
+    const s = readStream
+      .pipe(es.split())
+      .pipe(
+        es.mapSync(function (line: string) {
+          s.pause();
+          count++;
+          const lineArray = line.split(',');
+          writeStream.write(
+            JSON.stringify({
+              timestamp: lineArray[0],
+              transactionType: lineArray[1],
+              token: lineArray[2],
+              amount: lineArray[3],
+            }) + '\n',
+          );
+          s.resume();
+        }),
+      )
+      .on('error', function (err) {
+        console.log('Error while reading file.', err);
+      })
+      .on('end', function () {
+        console.log('Read entire file', count);
       });
-    }
-    return await streamReader(readStream);
   }
 }
