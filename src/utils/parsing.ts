349ea1314079d8d6bd176fddb2c6ccafb1d51903
Samiur-UTI <samiurkhan94@gmail.com>
2022-05-11 00:54:57 +0600

    caching init

--- src/utils/parsing.ts
+++ src/utils/parsing.ts
@@ -1,38 +1,46 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { Injectable } from '@nestjs/common';
+import { Injectable, CACHE_MANAGER, Inject } from '@nestjs/common';
 import { Transactions } from '../entities/transaction.entity';
+import { Cache } from 'cache-manager';
 @Injectable()
 export class ParsingService {
-  constructor() {}
-  parse(data: string): Transactions[] {
-    const dataArray = data.toString().split('\n');
-    const dataArrayLength = dataArray.length;
-    const chunk = [];
-    for (let i = 0; i < dataArrayLength; i++) {
-      const dataArraySplit = dataArray[i].split(',');
-      const dataArraySplitLength = dataArraySplit.length;
-      const transaction = {};
-      for (let j = 0; j < dataArraySplitLength; j++) {
-        if (j === 0) {
-          transaction['timestamp'] = Number(dataArraySplit[j])
-            ? Number(dataArraySplit[j])
-            : 0;
-        } else if (j === 1) {
-          transaction['transactionType'] =
-            dataArraySplit[j] === 'transaction_type'
-              ? 'DEPOSIT'
-              : dataArraySplit[j];
-        } else if (j === 2) {
-          transaction['token'] =
-            dataArraySplit[j] === 'token' ? '' : dataArraySplit[j];
-        } else if (j === 3) {
-          transaction['amount'] = parseFloat(dataArraySplit[j])
-            ? parseFloat(dataArraySplit[j])
-            : 0;
-        }
-      }
-      chunk.push(transaction);
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+  parse(data: string): any {
+    let trimmedEnd = '';
+    let dataArray = data.toString().split('\n');
+    if (dataArray[dataArray.length - 1] === '') {
+      dataArray = dataArray;
+    } else {
+      trimmedEnd = dataArray.pop()
+      
     }
-    return chunk;
+    // const dataArrayLength = dataArray.length;
+    // const chunk = [];
+    // for (let i = 0; i < dataArrayLength; i++) {
+    //   const dataArraySplit = dataArray[i].split(',');
+    //   const dataArraySplitLength = dataArraySplit.length;
+    //   const transaction = {};
+    //   for (let j = 0; j < dataArraySplitLength; j++) {
+    //     if (j === 0) {
+    //       transaction['timestamp'] = Number(dataArraySplit[j])
+    //         ? Number(dataArraySplit[j])
+    //         : 0;
+    //     } else if (j === 1) {
+    //       transaction['transactionType'] =
+    //         dataArraySplit[j] === 'transaction_type'
+    //           ? 'DEPOSIT'
+    //           : dataArraySplit[j];
+    //     } else if (j === 2) {
+    //       transaction['token'] =
+    //         dataArraySplit[j] === 'token' ? '' : dataArraySplit[j];
+    //     } else if (j === 3) {
+    //       transaction['amount'] = parseFloat(dataArraySplit[j])
+    //         ? parseFloat(dataArraySplit[j])
+    //         : 0;
+    //     }
+    //   }
+    //   chunk.push(transaction);
+    // }
+    // return chunk;
   }
 }
