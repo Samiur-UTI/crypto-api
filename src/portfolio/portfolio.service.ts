621a345f2560222b3c861652a91cd53b63fefe49
Samiur-UTI <samiurkhan94@gmail.com>
2022-05-11 20:52:12 +0600

    portfolio endpoint integration left

--- src/portfolio/portfolio.service.ts
+++ src/portfolio/portfolio.service.ts
@@ -20,7 +20,65 @@ export class PortfolioService {
     private transactionsRepository: Repository<Transactions>,
     private connection: Connection,
   ) {}
-  async get(): Promise<string> {
-    return 'PortfolioService';
+  async get(): Promise<Record<string, unknown>> {
+    const btcDeposit = await this.connection
+      .getRepository(Transactions)
+      .createQueryBuilder('transactions')
+      .select('SUM(amount)', 'amount')
+      .where('token = :token', { token: 'BTC' })
+      .andWhere('transactionType = :transactionType', {
+        transactionType: 'DEPOSIT',
+      })
+      .getRawOne();
+    const btcWithdrawal = await this.connection
+      .getRepository(Transactions)
+      .createQueryBuilder('transactions')
+      .select('SUM(amount)', 'amount')
+      .where('token = :token', { token: 'BTC' })
+      .andWhere('transactionType = :transactionType', {
+        transactionType: 'WITHDRAWAL',
+      })
+      .getRawOne();
+    const ethDeposit = await this.connection
+      .getRepository(Transactions)
+      .createQueryBuilder('transactions')
+      .select('SUM(amount)', 'amount')
+      .where('token = :token', { token: 'ETH' })
+      .andWhere('transactionType = :transactionType', {
+        transactionType: 'DEPOSIT',
+      })
+      .getRawOne();
+    const ethWithdrawal = await this.connection
+      .getRepository(Transactions)
+      .createQueryBuilder('transactions')
+      .select('SUM(amount)', 'amount')
+      .where('token = :token', { token: 'ETH' })
+      .andWhere('transactionType = :transactionType', {
+        transactionType: 'WITHDRAWAL',
+      })
+      .getRawOne();
+    const xrpDeposit = await this.connection
+      .getRepository(Transactions)
+      .createQueryBuilder('transactions')
+      .select('SUM(amount)', 'amount')
+      .where('token = :token', { token: 'XRP' })
+      .andWhere('transactionType = :transactionType', {
+        transactionType: 'DEPOSIT',
+      })
+      .getRawOne();
+    const xrpWithdrawal = await this.connection
+      .getRepository(Transactions)
+      .createQueryBuilder('transactions')
+      .select('SUM(amount)', 'amount')
+      .where('token = :token', { token: 'XRP' })
+      .andWhere('transactionType = :transactionType', {
+        transactionType: 'WITHDRAWAL',
+      })
+      .getRawOne();
+    return {
+      btcPortfolio: btcDeposit.amount - btcWithdrawal.amount,
+      ethPortfolio: ethDeposit.amount - ethWithdrawal.amount,
+      xrpPortfolio: xrpDeposit.amount - xrpWithdrawal.amount,
+    };
   }
 }
